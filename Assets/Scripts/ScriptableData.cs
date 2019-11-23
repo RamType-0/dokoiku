8a0919bf55c21192c65b54f63c5b01f4c284717e
kuni-0930 <niku936@gmail.com>
2019-11-23 19:47:28 +0900

    やり直し

--- Assets/Scripts/ScriptableData.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "MyScriptableObject/Create ScriptableData")]
-public class ScriptableData : ScriptableObject
-{
-    public GameObject cardPrefab;
-}
